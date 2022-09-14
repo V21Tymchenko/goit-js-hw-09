5f72476746035bedfe82be2489ab8752ea1a2db6
Valeriia tymchenko <lera.tymchenko@ukr.net>
2022-09-14 19:15:23 +0300

    fix bag with TimerId in task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,4 @@
+//  знаходимо силку на кнопки та боді
 const refs = {
   btnStart: document.querySelector('button[data-start]'),
   btnStop: document.querySelector('button[data-stop]'),
@@ -9,20 +10,21 @@ const CHANGE_COLOR_DELEY = 1000;
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
+// слушач на кнопці для старту зміни кольору
 refs.btnStart.addEventListener('click', () => {
   timerId = setInterval(() => {
     onClickChangeBodyColor();
   }, CHANGE_COLOR_DELEY);
 });
 
+// стивимо неактивість кнопку при зміні кольору боді
 function onClickChangeBodyColor() {
   refs.bodyRef.style.backgroundColor = getRandomHexColor();
   if (!refs.btnStart.hasAttribute('disabled')) {
     refs.btnStart.setAttribute('disabled', 'true');
   }
 }
-
+// слушач на кнопці для зупинки
 refs.btnStop.addEventListener('click', stopChangeColor);
 
 function stopChangeColor() {
